a12fa9438e18205b24e41aed1b5c36f3a1ad4e80
sierratango34 <k.sritawan@gmail.com>
2021-04-17 15:55:57 +0100

    skeleton of search form for an item within x miles of postcode

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
 
-export default function Search() {
-  return <h2>This is the search page</h2>;
+const handleSubmit = (event: { preventDefault: () => void; }) => {
+  event.preventDefault();
+  alert('You have submitted the form.')
 }
+
+const Search = () => (
+  <div>
+    <form action="/" method="post" onSubmit={handleSubmit}>
+      <fieldset>
+        <p>I want to</p>
+        <label>
+          <select placeholder="Please choose an option" name="type">
+            <option value="">--Please choose an option--</option>
+            <option value="share">share</option>
+            <option value="borrow">borrow</option>
+          </select>
+        </label>
+        <p>a</p>
+        <label>
+          <span className="visually-hidden">item</span>
+          <input type="text"
+            id="item-searchbox"
+            placeholder="item"
+            name="item" />
+        </label>
+        <label>
+          <p>within</p>
+          <select name="type">
+            <option value="">--Please choose an option--</option>
+            <option value="5">5 miles</option>
+            <option value="10">10 miles</option>
+          </select>
+        </label>
+        <p>of</p>
+        <label>
+          <span className="visually-hidden">postcode</span>
+          <input type="text"
+            id="postcode-searchbox"
+            placeholder="postcode"
+            name="postcode" />
+        </label>
+      </fieldset>
+      <button type="submit">Submit</button>
+    </form>
+  </div >
+);
+
+
+export default Search;
