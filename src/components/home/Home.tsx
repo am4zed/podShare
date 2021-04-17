54bb6d76a68395d7fa4cc0413711e7a5ca0a2fc9
Katharinabitzan <katharinabitzan@gmail.com>
2021-04-17 20:46:05 +0100

    edit styling for homepage in progress

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './home.scss';
 
   export default function Home() {
     return <div className='main'>
             <div className ='home'>
-            <section>Section 1
-              <h2 className ='home__heading'>Welcome SARAH</h2>
-              <p>Share the planet, build a community</p>
+              <section className ='home__section'>Section 1
+                <h2 className ='home__heading'>Welcome Sarah</h2>
+                <p className = 'home__slogan'>Share the planet, build a community</p>
+                </section>
+              <section className ='home__section'>Section 2
+                <h2 className ='home__heading'>Select if you want to search/offer</h2>
+                <div><Link to="/search">I need / Search</Link></div>
+                <div><Link to="/offer">I have / Share</Link></div>
               </section>
-            <section>Section 2
-              <h2 className ='home__heading'>Select if you want to search/offer</h2>
-              <div><Link to="/search">I need / Search</Link></div>
-              <div><Link to="/offer">I have / Share</Link></div>
-            </section>
-            <section>Section 3
-              <h2 className ='home__heading'>Browse categories</h2>
-              <div><Link to="/categories/sports">Sports</Link></div>
-              <div><Link to="/categories/cooking">Cooking</Link></div>
-              <div><Link to="/categories/diy">DIY</Link></div>
-              <div><Link to="/categories/kids">Kids and Baby</Link></div>
-            </section>
-            <section >Section 4
-              <h2 className ='home__heading'>Browse Pods</h2>
-            </section>
+              <section className ='home__section'>Section 3
+                <h2 className ='home__heading'>Browse categories</h2>
+                <div className='home__categorydiv'><Link to="/categories/sports">Sports</Link></div>
+                <div className='home__categorydiv'><Link to="/categories/cooking">Cooking</Link></div>
+                <div className='home__categorydiv'><Link to="/categories/diy">DIY</Link></div>
+                <div className='home__categorydiv'><Link to="/categories/kids">Kids and Baby</Link></div>
+              </section>
+              <section className ='home__section'>Section 4
+                <h2 className ='home__heading'>Browse Pods</h2>
+              </section>
             </div>
           </div>;
   }
