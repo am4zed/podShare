87c33cfe78d50c9823414e676b061aee4c19a807
Amy Edwardson <47303250+am4zed@users.noreply.github.com>
2021-04-17 15:59:07 +0100

    adds palette color variables and example of usage in pod-profile.scss

--- src/components/podProfile/PodProfile.tsx
+++ src/components/podProfile/PodProfile.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { User } from '../../model/User';
 
+import './pod-profile.scss';
+
 export interface PodProfileProps {
   user: User;
 }
 
 export class PodProfile extends React.Component {
-  private selected = false;
-
   render() {
     return (
       <div className='main'>
-        I am pod profile
-        <button onClick={() => this.toggleSelected()}></button>
+        <div className='pod-profile'>
+          <h2 className='pod-profile__heading'>Outdoors Pod</h2>
+          <div className='pod-profile__items'>
+            <h2 className='pod-profile__heading'>Items</h2>
+            {/* TODO - create Item component that can also be used in UserProfile.tsx */}
+          </div>
+          <div className='pod-profile__members'>
+            <h2 className='pod-profile__heading'>Members</h2>
+            {/* TODO - create PodMemberCard component  */}
+          </div>
+        </div>
+        <div className='button-container'>
+          <button className='button-container__join-button'>Join</button>
+          <button className='button-container__join-button2'>Join</button>
+        </div>
       </div>
     );
-  }
-
-  private toggleSelected() {
-    this.selected = !this.selected;
   }
 }
