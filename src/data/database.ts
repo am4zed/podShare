0c6de1991f3c76c205fcc5bf978e5c0358db9b00
Becky N <47067290+beckyjn@users.noreply.github.com>
2021-04-17 19:56:05 +0100

    usercards done

---
+++ src/data/database.ts
@@ -0,0 +1,14 @@
+import { User } from '../model/User';
+
+export const users: User[] = [
+  {
+    id: '7895',
+    name: 'bob',
+    bio: 'lorem ipsum',
+    rating: 3,
+    pods: [],
+    image: 'blah',
+    items: [],
+    location: 'EH1'
+  }
+];
