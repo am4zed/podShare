0d777948ef5145b640f3aafdcad1af9ce2e335e9
Katharinabitzan <katharinabitzan@gmail.com>
2021-04-17 15:12:03 +0100

    add home and login component first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { observer } from 'mobx-react';
 import { AppState } from './AppState';
 import { PodProfile } from './components/podProfile/podProfile';
 import Search from './components/search';
+import Home from './components/Home';
+import Login from './components/Login';
 
 @observer
 export class App extends React.PureComponent {
@@ -15,7 +17,8 @@ export class App extends React.PureComponent {
           <nav>
             <ul>
               <li><Link to="/search">Search</Link></li>
-              <li><Link to="/">Home</Link></li>
+              <li><Link to="/home">Home</Link></li>
+              <li><Link to="/login">Login</Link></li>
             </ul>
           </nav>
           <div>
@@ -23,7 +26,11 @@ export class App extends React.PureComponent {
               <Route path="/search">
                 <Search />
               </Route>
-              <Route path="/">
+              <Route path="/home">
+                <Home />
+              </Route>
+              <Route path="/login">
+                <Login />
               </Route>
             </Switch>
           </div>
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+  export default function Home() {
+    return <div>
+            <h2>This is the home/landing page</h2>
+            <h2>Welcome SARAH</h2>
+          </div>;
+  }
