1686956db0451638a2e86a9a133a4225521b85b9
Becky N <47067290+beckyjn@users.noreply.github.com>
2021-04-18 14:34:21 +0100

    basic styling applied to home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -16,24 +16,29 @@ export class Home extends React.Component {
             <h2 className='home__heading'>Good evening, Sarah</h2>
             <p className='home__slogan'>Build a pod, share the planet</p>
           </section>
-          <section className='home__needhave-grid'>
-            <h2 className='home__heading'>Select if you want to search/offer</h2>
-            <Link to='/search'>I need / Search</Link>
-            <Link to='/offer'>I have / Share</Link>
+          <section className='home__section'>
+            <h3 className='home__subheading'>What do you want to do?</h3>
+            <div className='home__needhave-grid'>
+              <Link to='/search'>I need / Search</Link>
+              <Link to='/offer'>I have / Share</Link>
+            </div>
           </section>
-          <section className='home__category-grid'>
-            <h2 className='home__heading'>Browse categories</h2>
-
-            <Link to='/categories/sports'>Sports</Link>
-            <Link to='/categories/cooking'>Cooking</Link>
-            <Link to='/categories/diy'>DIY</Link>
-            <Link to='/categories/kids'>Kids and Baby</Link>
+          <section className='home__section'>
+            <h3 className='home__subheading'>Browse categories</h3>
+            <div className='home__category-grid'>
+              <Link to='/categories/sports'>Sports</Link>
+              <Link to='/categories/cooking'>Cooking</Link>
+              <Link to='/categories/diy'>DIY</Link>
+              <Link to='/categories/kids'>Kids and Baby</Link>
+            </div>
           </section>
-          <section className='home__pod-grid'>
-            <h2 className='home__heading'>Browse Pods near you</h2>
-            <PodCard pod={pods[0]} />
-            <PodCard pod={pods[2]} />
-            <PodCard pod={pods[4]} />
+          <section className='home__section'>
+            <h3 className='home__subheading'>Browse Pods near you</h3>
+            <div className='home__pod-grid'>
+              <PodCard pod={pods[0]} />
+              <PodCard pod={pods[2]} />
+              <PodCard pod={pods[4]} />
+            </div>
           </section>
         </div>
       </div>
