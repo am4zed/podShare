9f1b4722c2a8158e8fd3c9b46799458e7faa8b88
Becky N <47067290+beckyjn@users.noreply.github.com>
2021-04-18 13:39:51 +0100

    trying to sort styling

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Pod } from '../../model/Pod';
 import { User } from '../../model/User';
-import { users } from '../../data/database';
+import { users, pods } from '../../data/database';
 
 import './home.scss';
+import { PodCard } from '../podCard/PodCard';
 
 export class Home extends React.Component {
   render() {
@@ -15,44 +16,24 @@ export class Home extends React.Component {
             <h2 className='home__heading'>Good evening, Sarah</h2>
             <p className='home__slogan'>Build a pod, share the planet</p>
           </section>
-          <section className='home__section'>
+          <section className='home__needhave-grid'>
             <h2 className='home__heading'>Select if you want to search/offer</h2>
-            <div className='home__needhavediv'>
-              <Link to='/search'>I need / Search</Link>
-            </div>
-            <div className='home__needhavediv'>
-              <Link to='/offer'>I have / Share</Link>
-            </div>
+            <Link to='/search'>I need / Search</Link>
+            <Link to='/offer'>I have / Share</Link>
           </section>
-          <section className='home__section'>
+          <section className='home__category-grid'>
             <h2 className='home__heading'>Browse categories</h2>
-            <div className='home__categorydiv'>
-              <Link to='/categories/sports'>Sports</Link>
-            </div>
-            <div className='home__categorydiv'>
-              <Link to='/categories/cooking'>Cooking</Link>
-            </div>
-            <div className='home__categorydiv'>
-              <Link to='/categories/diy'>DIY</Link>
-            </div>
-            <div className='home__categorydiv'>
-              <Link to='/categories/kids'>Kids and Baby</Link>
-            </div>
+
+            <Link to='/categories/sports'>Sports</Link>
+            <Link to='/categories/cooking'>Cooking</Link>
+            <Link to='/categories/diy'>DIY</Link>
+            <Link to='/categories/kids'>Kids and Baby</Link>
           </section>
-          <section className='home__section'>
+          <section className='home__pod-grid'>
             <h2 className='home__heading'>Browse Pods near you</h2>
-            <div className='home__categorydiv'>
-              <Link to='/pods/munrobaggers'>Munrobaggers</Link>
-            </div>
-            <div className='home__categorydiv'>
-              <Link to='/pods/cookingfiesta'>CookingFiesta</Link>
-            </div>
-            <div className='home__categorydiv'>
-              <Link to='/pods/codeclan'>CodeClan</Link>
-            </div>
-            <div className='home__categorydiv'>
-              <Link to='/pods/leithlinkshare'>Leith Links Share</Link>
-            </div>
+            <PodCard pod={pods[0]} />
+            <PodCard pod={pods[2]} />
+            <PodCard pod={pods[4]} />
           </section>
         </div>
       </div>
