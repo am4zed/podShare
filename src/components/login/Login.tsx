6b4af832c469c0a0e6f79591bdb22d4e9c0c0bdb
Becky N <47067290+beckyjn@users.noreply.github.com>
2021-04-18 14:52:37 +0100

    login style changes
    page background green (can't work out how to make anything else change)

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <form className='login__form'>
-      <label>
-        <p>Username</p>
-        <input type='text' />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type='password' />
-      </label>
-      <div>
-        <button type='submit'>Submit</button>
-      </div>
-    </form>
+    <div className='main'>
+      <form className='login__form'>
+        <label>
+          <p>Username</p>
+          <input type='text' />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type='password' />
+        </label>
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
+    </div>
   );
 }
