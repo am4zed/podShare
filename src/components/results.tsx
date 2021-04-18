ca6d210b99993a0cfaf69117a32b6d6ae6212262
sierratango34 <k.sritawan@gmail.com>
2021-04-18 11:15:31 +0100

    results page file start

---
+++ src/components/results.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {}
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
