a2272961df61b3c6a34e0ee8ea5840c5c818ebe0
Amy Edwardson <47303250+am4zed@users.noreply.github.com>
2021-04-18 15:39:50 +0100

    user profile looking ok

--- src/components/userCard/UserCard.tsx
+++ src/components/userCard/UserCard.tsx
@@ -12,19 +12,21 @@ export class UserCard extends React.Component<UserCardProps> {
     const { user } = this.props;
     return (
       <div className='user-card'>
-        <h2 className='user-card__name'>{user.name}</h2>
         <img className='user-card__image' src={user.image}></img>
-        <ul className='user-card__bio'>
-          <li>
-            <span className='label'>Rating:</span> {this.renderRatingStars(user.rating)}
-          </li>
-          <li>
-            <span className='label'>Location:</span> {user.location}
-          </li>
-          <li>
-            <span className='label'>Bio: {user.bio}</span>{' '}
-          </li>
-        </ul>
+        <div className='item-card__details-panel'>
+          <h2 className='user-card__name'>{user.name}</h2>
+          <ul className='user-card__info-list'>
+            <li>
+              <span className='label'>Rating:</span> {this.renderRatingStars(user.rating)}
+            </li>
+            <li>
+              <span className='label'>Location:</span> {user.location}
+            </li>
+            <li>
+              <span className='label'>Bio: {user.bio}</span>{' '}
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/components/userProfile/userProfile.tsx
+++ src/components/userProfile/userProfile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { UserCard } from '../userCard/UserCard';
 
-import { pods, users } from '../../data/database';
+import { items, pods, users } from '../../data/database';
 import { PodCard } from '../podCard/PodCard';
+import { ItemCard } from '../item/ItemCard';
 
 import './user-profile.scss';
 
@@ -10,26 +11,33 @@ export class UserProfile extends React.Component {
   render() {
     const user = users[0];
     return (
-      <div className='main'>
-        <div className='user-profile'>
-          <UserCard user={user} />
+      <div className='user-profile'>
+        <div className='pod-profile__body'>
+          <div className='user-profile__hero'>
+            <div className='grid-container'>
+              <UserCard user={user} />
+            </div>
+          </div>
 
           <div className='user-profile__items'>
             <h2 className='user-profile__heading'>{user.name}'s Items</h2>
-
-            <div className='user-profile__itemlist'>
-              <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-              </ul>
+            <div className='grid-container'>
+              <div className='user-profile__item-grid'>
+                {items.map((item) => (
+                  <ItemCard key={item.id} item={item} />
+                ))}
+              </div>
             </div>
           </div>
+
           <div className='user-profile__pods'>
             <h2 className='user-profile__heading'>{user.name}'s Pods</h2>
-            <div className='user-profile__podlist'>
-              <PodCard pod={pods[0]} />
-              <PodCard pod={pods[4]} />
+            <div className='grid-container'>
+              <div className='user-profile__pod-grid'>
+                <PodCard pod={pods[0]} />
+                <PodCard pod={pods[2]} />
+                <PodCard pod={pods[4]} />
+              </div>
             </div>
           </div>
         </div>
